1bf749243c9f9c30eefd6e3dfb207fef7678dfb4
julienmb3-dot <julienmb3@gmail.com>
2025-12-03 19:17:40 +0100

    Implementation du base.html index.html avec succès

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-import math
+from flask import Flask, render_template, request, session, redirect, url_for
+import pymongo
+
+from datetime import datetime
+
+
+client = pymongo.MongoClient("mongodb+srv://b:7AoQ4qDkgmsywcBs@cluster0.0s76g90.mongodb.net/?appName=Cluster0")
+db = client["SitePostIt"]
+
+app = Flask(__name__)
+
+app.secret_key = "akndjbwkdgaboqudbkoa"
+
+@app.route("/")
+def index():
+    data = list(db["PostIt"].find())
+    return render_template("index.html", annonces=data)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+if __name__  == "__main__":
+    app.run(host="0.0.0.0", port=82)

ebd511990f274ccd610d09e2ea498b577ada54dd
julienmb3-dot <julienmb3@gmail.com>
2025-12-10 19:19:06 +0100

    login, sigin, un peu de la page publish

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import pymongo
 
-from datetime import datetime
+import datetime
 
 
 client = pymongo.MongoClient("mongodb+srv://b:7AoQ4qDkgmsywcBs@cluster0.0s76g90.mongodb.net/?appName=Cluster0")
@@ -15,6 +15,70 @@ app.secret_key = "akndjbwkdgaboqudbkoa"
 def index():
     data = list(db["PostIt"].find())
     return render_template("index.html", annonces=data)
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        db_users = db["Users"]
+        user = db_users.find_one({"user_id" : request.form["user_id"]})
+        if user:
+            if request.form["password"] == user["password"]:
+                session["user"] = request.form["user_id"]
+                return redirect(url_for("index"))
+            else:
+                return render_template('login.html', erreur="mot de passe incorrect")
+        else:
+            return render_template('login.html', erreur="utilisateur incorrect")
+    else:
+        return render_template('login.html')
+
+@app.route("/sigin", methods = ["POST", "GET"])
+def sigin():
+    if request.method == "POST":
+        db_users = db["Users"]
+        new_user = db_users.find_one({"user_id" : request.form["user_id"]})
+        if new_user:
+            return render_template('sigin.html', erreur="Ce nom d'utilisateur est déjà pris.")
+        else:
+            if request.form["password"] == request.form["password_validation"]:
+                if not(request.form["password"] == "" or request.form["user_id"] == ""):
+                    db_users.insert_one({
+                        "user_id" : request.form["user_id"],
+                        "password" : request.form["password"]
+                    })
+                    session["user"] = request.form["user_id"]
+                    return redirect(url_for("index"))
+                else:
+                    return render_template('sigin.html', erreur="Le nom d'utilisateur ou le mot de passe est vide.")
+            else:
+                return render_template('sigin.html', erreur="Les deux mots de passe ne correspondent pas.")
+    else:
+        return render_template("sigin.html")
+    
+@app.route("/publish", methods = ["POST", "GET"])
+def publish():
+    if "user" not in session:
+        return redirect(url_for("login.html"))
+    
+    if request.method == "POST":
+        db_postIt = db["PostIt"]
+        content = request.form["content"]
+        auteur = session["user"]
+        date = datetime.datetime.now()
+        date = date.strftime("%x")
+        color = "yellow"
+        if content:
+            db_postIt.insert_one({
+                "content" : content,
+                "auteur" : auteur,
+                "color" : color,
+                "date" : date
+            })
+            return redirect(url_for("index"))
+        else:
+            return render_template('publish.html', erreur="Le titre ou le contenu est vide")
+    else:
+        return render_template("publish.html")
 
 @app.route("/logout")
 def logout():
